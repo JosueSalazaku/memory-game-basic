f4d8b68d79d2a8e432434fdd601ceba22126d02d
Josue Sebastiao <josueseba@Josue-2.local>
2023-03-13 14:19:06 +0100

    init

---
+++ app.js
@@ -0,0 +1,52 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    }
+    {
+        name: 'cheesburger',
+        img: 'images/cheesburger.png',
+    }
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    }
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    }
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    }
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    }
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    }
+    {
+        name: 'cheesburger',
+        img: 'images/cheesburger.png',
+    }
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    }
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    }
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    }
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    }
+]
+
+console.log(cardArray)
