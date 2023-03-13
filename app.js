728fec78e91297d47791835d5cccc60a77b450f2
Josue Sebastiao <josueseba@Josue-2.local>
2023-03-13 18:40:53 +0100

    init

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cardArray = [
         img: 'images/fries.png',
     },
     {
-        name: 'cheesburger',
-        img: 'images/cheesburger.png',
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
     },
     {
         name: 'hotdog',
@@ -28,8 +28,8 @@ const cardArray = [
         img: 'images/fries.png',
     },
     {
-        name: 'cheesburger',
-        img: 'images/cheesburger.png',
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
     },
     {
         name: 'hotdog',
@@ -52,13 +52,39 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const grid = document.querySelector('#grid')
+const cardsChosen = []
+const cardsChosenIds = []
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
       card.setAttribute('src', 'images/blank.png')
       card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
       grid.appendChild(card)
     }
 }
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    console.log(cards) 
+    console.log('check for Match !')
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You have a match my boyy')
+        cards[cardsChosenIds[0]].setAttribute('src', images/white.png)
+    }
+}
+
+function flipCard() {
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    console.log(cardsChosen)
+    console.log(cardsChosenIds)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
