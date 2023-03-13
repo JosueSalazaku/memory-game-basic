b61ca6697855018dccbd74cdc5a3048d3c82a358
Josue Sebastiao <josueseba@Josue-2.local>
2023-03-13 14:22:30 +0100

    app.js

--- app.js
+++ app.js
@@ -2,51 +2,51 @@ const cardArray = [
     {
         name: 'fries',
         img: 'images/fries.png',
-    }
+    },
     {
         name: 'cheesburger',
         img: 'images/cheesburger.png',
-    }
+    },
     {
         name: 'hotdog',
         img: 'images/hotdog.png',
-    }
+    },
     {
         name: 'ice-cream',
         img: 'images/ice-cream.png',
-    }
+    },
     {
         name: 'milkshake',
         img: 'images/milkshake.png',
-    }
+    },
     {
         name: 'pizza',
         img: 'images/pizza.png',
-    }
+    },
     {
         name: 'fries',
         img: 'images/fries.png',
-    }
+    },
     {
         name: 'cheesburger',
         img: 'images/cheesburger.png',
-    }
+    },
     {
         name: 'hotdog',
         img: 'images/hotdog.png',
-    }
+    },
     {
         name: 'ice-cream',
         img: 'images/ice-cream.png',
-    }
+    },
     {
         name: 'milkshake',
         img: 'images/milkshake.png',
-    }
+    },
     {
         name: 'pizza',
         img: 'images/pizza.png',
     }
 ]
 
-console.log(cardArray)
+cardArray.sort(() => 0.5 - Math.random())
